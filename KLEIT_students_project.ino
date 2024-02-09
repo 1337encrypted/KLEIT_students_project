8de12e44791bfc9fb816eb036920dcde4b596e16
1337encrypted <smallaszero@gmail.com>
2024-02-09 12:09:56 +0530

    Added potentiometer

--- KLEIT_students_project.ino
+++ KLEIT_students_project.ino
@@ -1,22 +1,21 @@
 #include <TimerOne.h>
 
 // Define PWM pin and relay pins
-constexpr uint8_t relayPin1 = 3;
-constexpr uint8_t relayPin2 = 4;
+constexpr uint8_t relayPin1 = 5;
+constexpr uint8_t relayPin2 = 6;
 constexpr uint8_t pwmPin = 9;
+constexpr uint8_t buzzPin = 2;
+constexpr uint8_t redLed = 4;
+constexpr uint8_t blueLed = 3;
+constexpr uint8_t potPin = A1;
 
-static const uint8_t buf_len = 5;
-char receivedChar;
-char buf[buf_len];
-uint8_t idx = 0;
 int Speed = 0;
-double volts = 0.00;
 
 enum class motorStates : char {
   FRONT = 'F',
   BACK = 'B'
 };
-motorStates motorStatus = motorStates::FRONT;
+motorStates motorStatus = motorStates::BACK;
 
 void setup() {
   Serial.begin(9600);
@@ -24,6 +23,14 @@ void setup() {
   pinMode(pwmPin, OUTPUT);
   pinMode(relayPin1, OUTPUT);
   pinMode(relayPin2, OUTPUT);
+  pinMode(buzzPin, OUTPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(blueLed, OUTPUT);
+  pinMode(potPin, INPUT);
+
+  digitalWrite(buzzPin, LOW);
+  digitalWrite(redLed, LOW);
+  digitalWrite(blueLed, LOW);
 
   // Set up Timer1 to generate a 2kHz PWM signal on pin 5
   Timer1.initialize(500);  // 500 microseconds (2kHz)
@@ -42,12 +49,6 @@ void back() {
   Serial.print("Back: ");
 }
 
-void clearBuffer() {
-  for (int i = 0; i < buf_len; i++) {
-    buf[i] = 0;
-  }
-}
-
 motorStates charToMotorStates(char state) {
   switch (state) {
     case 'F': return motorStates::FRONT;
@@ -58,32 +59,15 @@ motorStates charToMotorStates(char state) {
 }
 
 void loop() {
-  while ( {
-    receivedChar = Serial.read();
 
-    if (receivedChar == 'F' || receivedChar == 'f' || receivedChar == 'B' || receivedChar == 'b') {
-      motorStatus = charToMotorStates(receivedChar);
-    } else if (receivedChar == '\n') {
-      Speed = atoi(buf);
+  Speed = analogRead(potPin);
+  
+  Speed = map(Speed,0,1024,0,200);
 
-      if (idx > 4 || Speed > 1024) {
-        Serial.println("Invalid input, \nPlease enter values between 0 to 1024\nF or f for Front \nB or b for back.");
-        Speed = 0;
-      } else {
-        Speed = map(Speed, 0, 1024, 0, 76);  //Convert the voltage from 0v to 3.3v pwm signals
-        volts = map(Speed, 0, 76, 0, 3.3);
-        Timer1.pwm(pwmPin, Speed);  // PWM duty cycle (0 to 1023, where 0 is 0% and 1023 is 100%)
-      }
-      idx = 0;
-      clearBuffer();
-    } else {
-      // Only append if index is not over message limit
-      if (idx < buf_len - 1) {
-        buf[idx] = receivedChar;
-        idx++;
-      }
-    }
-  }
+  Timer1.pwm(pwmPin, Speed);
+  // analogWrite(redLed, Speed);
+  analogWrite(blueLed, Speed);
+
 
   switch (motorStatus) {
     case motorStates::FRONT:
@@ -93,7 +77,5 @@ void loop() {
       back();
       break;
   }
-
-  // Serial.println(Speed);
-  Serial.println(volts);
+  Serial.println(Speed);
 }
